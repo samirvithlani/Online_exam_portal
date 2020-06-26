1382390981214548b5faf96ad093129d6ec8677f
samirvithlani <samir.vithlani83955@gmail.com>
2020-06-26 16:21:15 +0530

    Update OnlineExamPortalApplication.java

--- src/main/java/com/OnlineExamPortalApplication.java
+++ src/main/java/com/OnlineExamPortalApplication.java
@@ -9,6 +9,8 @@ public class OnlineExamPortalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineExamPortalApplication.class, args);
 		System.out.println("hi");
+		System.out.println("hi");
+		
 	}
 
 }
