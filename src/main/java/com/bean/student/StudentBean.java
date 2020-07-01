e35b29e1461dbedab042eba385f7e51a6814351d
Hem-1112 <hempatel1234@gmail.com>
2020-07-01 13:30:49 +0530

    Institute Completed

--- src/main/java/com/bean/student/StudentBean.java
+++ src/main/java/com/bean/student/StudentBean.java
@@ -1,5 +1,64 @@
 package com.bean.student;
 
-public class StudentBean {
-
+public class StudentBean 
+{
+	private String sId;
+	private String fName;
+	private String lName;
+	private String sMail;
+	private long sContact;
+	private String uName;
+	private String uPassword;
+	private String iId;
+	
+	
+	public String getsId() {
+		return sId;
+	}
+	public void setsId(String sId) {
+		this.sId = sId;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getsMail() {
+		return sMail;
+	}
+	public void setsMail(String sMail) {
+		this.sMail = sMail;
+	}
+	public long getsContact() {
+		return sContact;
+	}
+	public void setsContact(long sContact) {
+		this.sContact = sContact;
+	}
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public String getuPassword() {
+		return uPassword;
+	}
+	public void setuPassword(String uPassword) {
+		this.uPassword = uPassword;
+	}
+	public String getiId() {
+		return iId;
+	}
+	public void setiId(String iId) {
+		this.iId = iId;
+	}
+	
 }
